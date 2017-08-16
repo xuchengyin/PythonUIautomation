caec95492dfb69ebee8d958f2b228cc566e6b2fc
xuchengyin <462641342@qq.com>
2017-08-16 09:10:47 +0800

    add code

--- UIautomation/Test/ExecuteAllTest.py
+++ UIautomation/Test/ExecuteAllTest.py
@@ -4,12 +4,15 @@ import unittest
 import sys
 import os
 import re
-
+import HTMLTestRunner
+import time
 
 def testSuite():
     # path = os.path.abspath(os.path.dirname(sys.argv[0]))
     # filesList = os.listdir(path)
-    # test=re.compile('test\.py{1}quot',re.IGNORECASE)
+    # test=re.compile('^test',re.IGNORECASE)
+    # files=filter(test.search,filesList)
+    # print files
     files = ['test01.py', 'test02.py', 'test03.py']
     filenameToModuleName = lambda f: os.path.splitext(f)[0]
     mous = map(filenameToModuleName, files)
@@ -20,4 +23,9 @@ def testSuite():
 
 
 if __name__ == '__main__':
-    unittest.main(defaultTest='testSuite')
+    now = time.strftime("%Y-%m-%M-%H_%M_%S", time.localtime(time.time()))
+    file='D:\\UI\\PythonUIautomation\\TestResult\\'+now+'report.html'
+    with(open(file,'wb')) as fp:
+        runner=HTMLTestRunner.HTMLTestRunner(stream=fp,title='test',description='test')
+        runner.run(testSuite())
+
